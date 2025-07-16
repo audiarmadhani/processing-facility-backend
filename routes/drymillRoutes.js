147659ba4329aff467dc0455b7a821cf40398ccb
Audi Armadhani <audiarmadhani@Aulias-MacBook-Air-9.local>
2025-07-16 11:17:37 +0700

    drymill rev3

--- routes/drymillRoutes.js
+++ routes/drymillRoutes.js
@@ -1078,7 +1078,7 @@ router.get('/dry-mill-data', async (req, res) => {
         pp."productLine" AS "productLine",
         COALESCE(pp."processingType", pd."processingType") AS "processingType",
         pd."lotNumber",
-        pp."referenceNumber",
+        pd."referenceNumber",
         CASE
           WHEN dm."entered_at" IS NOT NULL AND dm."exited_at" IS NULL THEN 'In Dry Mill'
           WHEN dm."exited_at" IS NOT NULL THEN 'Processed'
@@ -1113,7 +1113,7 @@ router.get('/dry-mill-data', async (req, res) => {
         pp.weight, rd.weight, pp.quality,
         pp.producer, rd.producer, rd."farmerName",
         pp."productLine", COALESCE(pp."processingType", pd."processingType"),
-        pd."lotNumber", pp."referenceNumber", pp.notes, rd.notes,
+        pd."lotNumber", pd."referenceNumber", pp.notes, rd.notes,
         pp."storedDate", rd.rfid,
         fm."farmVarieties",
         ldw.drying_weight
